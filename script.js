8d6ddc9827c8b7e10a193e29d0b31376920b3001
VV104-18 <holidaygg.backup@gmail.com>
2025-11-24 11:48:51 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function showMessage() {
   alert('Hello From JavaScript')
 }
+const heading = document.querySelector('h1')
+function changeTitle() {
+  heading.textContent = 'You have changed the tile!'
+}
